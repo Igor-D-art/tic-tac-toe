11bd85f0c2e1bff1b8a1390bc98e83ad38bd0db3
Igor-D-art <gorgsemenon1@gmail.com>
2022-01-04 14:16:49 +0200

    Fixed bugs, added a check for already occupied cells and a check for a draw

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,7 +2,7 @@ const view = (function(){
 
     const displayMove = (cell, marker)=> {
         let location = document.getElementById(cell);
-        location.innerText = marker;
+        location.innerText = marker; 
     };
 
     const displayMessage = (msg)=>{
@@ -32,7 +32,7 @@ const model = (function(){
     let markIdent = 2; // needed to make sure that the playerX gets the marker X and the playerO gets the marker O
     // please look in the Player factory function
 
-    let turn = 2;
+    let usedCells = [];
 
     const Player = function(){
         let marker;
@@ -46,37 +46,57 @@ const model = (function(){
 
         const makeMove = function(cell){
 
+            let allHits=[]; 
+            
+            model.usedCells.push(cell); // needed for checking if a location has been already occupied
+
             for(let i=0; i<combos.length; i++){
                 let combo = combos[i];
-                let hits = combo.hits;
                 let locations = combo.locations;
+                let hits = combo.hits;
                 let index = locations.indexOf(cell);
-
+               
                 if(index >= 0){
+                   
                     hits[index] = this.marker;
-                    view.displayMove(cell, this.marker);
-                    if (checkStatus(hits)){
+                
+                    if (checkWinner(hits)){
                         view.displayMessage (`Player ${this.marker} wins! Please restart the game!`);
                         model.gameOver=true;
                     };
                 };
+
+                allHits = allHits.concat(hits);
             };
-           
+
+            view.displayMove(cell, this.marker);
+
+            if (checkDraw(allHits)){
+                view.displayMessage (`It's a draw! Please restart the game!`);
+                model.gameOver=true;
+            };
         };
         return {marker, makeMove};
     };
 
-    const checkStatus = function(hits){
+    const checkWinner = function(hits){
       if (hits[0] === hits[1] && hits[1] === hits [2]) {
           return true;
       };
       return false;
     };
 
+    const checkDraw = function (allHits){
+        if (model.gameOver===false && allHits.indexOf('')===-1){
+            return true;
+        }
+       return false;
+    };
+
     const playerX = Player();
     const playerO = Player();
 
-    return {playerX, playerO, gameOver, boardSize, combos}
+    return {playerX, playerO, gameOver, boardSize, combos, usedCells}
 
 })();
 
@@ -85,25 +105,24 @@ const controller = (()=>{
     let turn = 2;
 
     const selectCell = function(tdID){    
-        if (!model.gameOver){ 
+        if (!model.gameOver && model.usedCells.indexOf(tdID)===-1){ 
             if (turn % 2 === 0){
                 view.displayMessage("Player O's turn!")
                 model.playerX.makeMove(tdID);
                 turn += 1;
-                
             } else {
                 view.displayMessage("Player X's turn!")
                 model.playerO.makeMove(tdID);
                 turn +=1;
             };
         } else {
+            view.displayMessage('Please select an empty cell.')
             return false;
         }
 
     };
 
    const restart = function(){
-        // alert('fucking restart');
         for(let i=0; i<model.boardSize; i++){
             let cell = document.getElementById(i);
             cell.innerHTML="";
@@ -115,12 +134,13 @@ const controller = (()=>{
                 hits[j]='';
             };
         };
-
+        turn=2;
+        view.displayMessage("Player X's turn!");
+        model.usedCells = [];
         model.gameOver = false;
    };
 
-
- return {selectCell, restart}
+ return {selectCell, restart, turn}
 })();
 
 
@@ -142,7 +162,3 @@ function init (){
 };
 
 init();
-
-
-
-
